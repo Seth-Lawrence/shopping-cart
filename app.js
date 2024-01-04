b32ba27198205eb211a40b3725d1dbe89d22148e
Jia Rong Gan <jiaronggan21@gmail.com>
2024-01-03 18:52:58 -0600

    added patch route

--- app.js
+++ app.js
@@ -21,9 +21,23 @@ app.post('/items', function (req, res) {
   console.log(item);
   items.push(item);
 
-
-  return res.json(item)
+  return res.json({added : item})
 });
+
+app.get("/items/:name", function(req, res){
+  const matchName = items.filter(item => item.name === req.params.name)
+  return res.json(matchName)
+})
+
+app.patch("/items/:name", function(req, res){
+  const item = items.filter(item => item.name === req.params.name)
+  item[0].name = req.body.name
+  item[0].price = req.body.price
+
+  const updatedItem = {name: item[0].name, price : item[0].price}
+  return res.json({updated: updatedItem})
+})
+
 
 module.exports = app;
 
